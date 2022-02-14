a80e9fae29cdffcc6583c72883146991c0801702
ansango <anibalsantosgo@gmail.com>
2022-02-14 17:18:08 +0100

    indext build

---
+++ src/components/Components/Cards/index.ts
@@ -0,0 +1,6 @@
+import Card from "./Card/Card";
+import CardAction from "./CardAction/CardAction";
+import CardImage from "./CardImage/CardImage";
+import CardHorizontal from "./CardHorizontal/CardHorizontal";
+
+export { Card, CardAction, CardImage, CardHorizontal };
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,20 @@ import { Spinner } from "./components/Atoms/Spinners";
 import { Accordion, AccordionFlush } from "./components/Components/Accordions";
 import { Alert, AlertContent } from "./components/Components/Alerts";
 import Breadcrumb from "./components/Components/Breadcrumb/Breadcrumb";
-import ButtonGroup from "./components/Components/ButtonGroup/ButtonGroup";
+
 import {
   Button,
   ButtonIcon,
   ButtonDuotone,
   ButtonMonochrome,
 } from "./components/Components/Buttons";
+
+import ButtonGroup from "./components/Components/ButtonGroup/ButtonGroup";
+
+// TODO: Still import cards
+import { Card, CardAction, CardHorizontal, CardImage } from "./components/Components/Cards";
+
+import { Dropdown, DropdownIcon } from "./components/Components/Dropdowns";
 
 export {
   Spinner,
@@ -22,4 +29,10 @@ export {
   ButtonDuotone,
   ButtonMonochrome,
   ButtonGroup,
+  Card,
+  CardAction,
+  CardHorizontal,
+  CardImage,
+  Dropdown,
+  DropdownIcon,
 };
